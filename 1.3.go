77b482fd77dd3d4c5abbeb0818db052c623450e3
srKazuya <prodeer99@gmail.com>
2025-08-16 15:07:17 +0300

    1.3

---
+++ 1.3.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var workers int = 10
+
+func main() {
+	ch := make(chan int)
+
+	wg := &sync.WaitGroup{}
+
+
+	for i := 0; i < int(workers); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for v := range ch {
+				fmt.Println(v)
+			}
+
+		}()
+	}
+
+	num := 0
+	for {
+		num++
+		ch <- num
+		time.Sleep(2 * time.Second)
+	}
+}
