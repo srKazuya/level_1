5e708107270838d3faa10a25d4234a4ded409699
srKazuya <prodeer99@gmail.com>
2025-08-26 18:32:40 +0300

    1.15

---
+++ 1.15/1.15.go
@@ -0,0 +1,16 @@
+package main
+
+var justString string
+
+func createHugeString(){
+	return 
+}
+
+func someFunc() {
+	v := createHugeString(1 << 10)
+	justString = string([]byte(v[:100]))
+}
+
+func main() {
+	someFunc()
+}
